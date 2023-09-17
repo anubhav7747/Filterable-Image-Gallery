45eef1c5cf49ad73b9d7951a001122c79f2cce44
anubhav7747 <anubhavtiwari007005@gmail.com>
2023-09-17 17:41:43 +0530

    updated script.js

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-(function () {
-    const buttons = document.querySelectorAll('.btn');
-    const images = document.querySelectorAll('.image');
+const buttons = document.querySelectorAll(".btn");
+const image = document.querySelectorAll(".image");
 
-    buttons.forEach((button) => {
-        button.addEventListener('click', (e) => {
-            e.preventDefault();
-            const filter = e.target.dataset.filter;
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const filter = e.target.dataset.filter;
 
-            images.forEach((item) => {
-                if (filter === 'all') {
-                    item.style.display = 'block';
-                } else {
-                    if (item.classList.contains(filter)) {
-                        item.style.display = 'block';
-                    } else {
-                        item.style.display = 'none';
-                    }
-                }
-            });
-        });
+    image.forEach((item) => {
+      if (filter === "all") {
+        item.style.display = "block";
+      } else {
+        if (item.classList.contains(filter)) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      }
     });
+  });
 });
